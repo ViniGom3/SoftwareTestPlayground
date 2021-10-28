63ef8cfb95391ffa78a7fbc698ea55ee9b6eab72
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-10-28 08:44:08 -0300

    refactor: put message error in a variable

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 module.exports = {
   // https://leetcode.com/problems/roman-to-integer/
   romanToInt: function (s) {
+    const messageError = "VALOR INVALIDO";
+
     if (typeof s === "number" || s.length === 0) {
-      return "VALOR INVALIDO";
+      return messageError;
     }
 
     const roman = {
@@ -24,18 +26,19 @@ module.exports = {
     }
 
     let response;
-    isNaN(resultado) ? (response = "VALOR INVALIDO") : (response = resultado);
+    isNaN(resultado) ? (response = messageError) : (response = resultado);
     return response;
   },
   // https://leetcode.com/problems/shuffle-the-array/
   shuffle: function (nums, n) {
-    if (!Array.isArray(nums)) return "VALOR INVALIDO";
+    const messageError = "VALOR INVALIDO";
+    if (!Array.isArray(nums)) return messageError;
     const array = nums.filter((item) => item);
-    if (array.length === 0) return "VALOR INVALIDO";
-    if (array.length !== 2 * n) return "VALOR INVALIDO";
-    if (array.length < n) return "VALOR INVALIDO";
-    if (n > 500) return "VALOR INVALIDO";
-    if (n === 0) return "VALOR INVALIDO";
+    if (array.length === 0) return messageError;
+    if (array.length !== 2 * n) return messageError;
+    if (array.length < n) return messageError;
+    if (n > 500) return messageError;
+    if (n === 0) return messageError;
     if (n === 1) return array;
     if (n === array.length) return array;
 
@@ -48,10 +51,11 @@ module.exports = {
   },
   // https://leetcode.com/problems/jewels-and-stones/
   numJewelsInStones: function (J, S) {
-    if (typeof J !== "string" || typeof S !== "string") return "VALOR INVALIDO";
-    if (new Set(J).size != J.length) return "VALOR INVALIDO";
-    if (J.length === 0) return "VALOR INVALIDO";
-    if (S.length >= 50) return "VALOR INVALIDO";
+    const messageError = "VALOR INVALIDO";
+    if (typeof J !== "string" || typeof S !== "string") return messageError;
+    if (new Set(J).size != J.length) return messageError;
+    if (J.length === 0) return messageError;
+    if (S.length >= 50) return messageError;
 
     const resultado = [];
     for (let i = 0; i < S.length; i++) {
